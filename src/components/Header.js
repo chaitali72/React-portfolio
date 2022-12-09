364a96399b10fc11e9f786ac2af3c85103f0927e
Chaitali <60978542+chaitali72@users.noreply.github.com>
2022-12-08 19:37:14 -0500

    update image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import profile from "./images/profile.jpg";
+import girl from "./images/girl.jpeg";
+
 export class Header extends Component {
   render() {
     return (
       <div>
         <header>
-          <img src={profile} alt="profile" className=" image_profile"></img>
+          <img src={girl} alt="profile" className=" image_profile"></img>
           <h1>Chaitali Mahida</h1>
         </header>
         <h2 className="header_title ">Full-Stack Software Developer</h2>
